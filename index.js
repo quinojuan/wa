c2cd6f0a4d5dd5a408d19aa9538efd2725d407b3
Juan Quino <quinojuan@gmail.com>
2023-11-10 19:14:22 -0300

    finished

--- index.js
+++ index.js
@@ -7,37 +7,47 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 const checkWeather = async (city) => {
   const response = await fetch(apiUrl + city + `&appid=${API_KEY}`);
-  var data = await response.json();
 
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
-  switch (data.weather[0].main) {
-    case "Clouds":
-      weatherIcon.src = "images/clouds.png";
-      break;
-    case "Clear":
-      weatherIcon.src = "images/clear.png";
-      break;
-    case "Rain":
-      weatherIcon.src = "images/rain.png";
-      break;
-    case "Drizzle":
-      weatherIcon.src = "images/drizzle.png";
-      break;
-    case "Mist":
-      weatherIcon.src = "images/mist.png";
-      break;
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    switch (data.weather[0].main) {
+      case "Clouds":
+        weatherIcon.src = "images/clouds.png";
+        break;
+      case "Clear":
+        weatherIcon.src = "images/clear.png";
+        break;
+      case "Rain":
+        weatherIcon.src = "images/rain.png";
+        break;
+      case "Drizzle":
+        weatherIcon.src = "images/drizzle.png";
+        break;
+      case "Mist":
+        weatherIcon.src = "images/mist.png";
+        break;
+    }
+    
+    // menos efectivo
+    // if (data.weather[0].main == "Clouds") weatherIcon.src = "images/clouds.png";
+    // else if (data.weather[0].main == "Clear") weatherIcon.src = "images/clear.png";
+    // else if (data.weather[0].main == "Rain") weatherIcon.src = "images/rain.png";
+    // else if (data.weather[0].main == "Drizzle") weatherIcon.src = "images/drizzle.png";
+    // else if (data.weather[0].main == "Mist") weatherIcon.src = "images/mist.png";
+    
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
-
-  // menos efectivo
-  // if (data.weather[0].main == "Clouds") weatherIcon.src = "images/clouds.png";
-  // else if (data.weather[0].main == "Clear") weatherIcon.src = "images/clear.png";
-  // else if (data.weather[0].main == "Rain") weatherIcon.src = "images/rain.png";
-  // else if (data.weather[0].main == "Drizzle") weatherIcon.src = "images/drizzle.png";
-  // else if (data.weather[0].main == "Mist") weatherIcon.src = "images/mist.png";
 };
 
 searchBtn.addEventListener("click", () => {
