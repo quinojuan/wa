b28827f5233f25461336a878d6b86cbea7824b85
Juan Quino <quinojuan@gmail.com>
2023-11-10 16:13:06 -0300

    apiKey resguardada

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const apiKey = "";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
 const checkWeather = async () => {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  const response = await fetch(apiUrl + `&appid=${API_KEY}`);
   var data = await response.json();
 
   console.log(data);
