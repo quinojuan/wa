0f5e58d71c54c6ee323a75182700651319d269e6
Juan Quino <quinojuan@gmail.com>
2023-11-10 18:14:46 -0300

    error de breaks en switch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const apiUrl =
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 const checkWeather = async (city) => {
   const response = await fetch(apiUrl + city + `&appid=${API_KEY}`);
@@ -13,7 +14,30 @@ const checkWeather = async (city) => {
   document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-  console.log(data);
+  switch (data.weather[0].main) {
+    case "Clouds":
+      weatherIcon.src = "images/clouds.png";
+      break;
+    case "Clear":
+      weatherIcon.src = "images/clear.png";
+      break;
+    case "Rain":
+      weatherIcon.src = "images/rain.png";
+      break;
+    case "Drizzle":
+      weatherIcon.src = "images/drizzle.png";
+      break;
+    case "Mist":
+      weatherIcon.src = "images/mist.png";
+      break;
+  }
+
+  // menos efectivo
+  // if (data.weather[0].main == "Clouds") weatherIcon.src = "images/clouds.png";
+  // else if (data.weather[0].main == "Clear") weatherIcon.src = "images/clear.png";
+  // else if (data.weather[0].main == "Rain") weatherIcon.src = "images/rain.png";
+  // else if (data.weather[0].main == "Drizzle") weatherIcon.src = "images/drizzle.png";
+  // else if (data.weather[0].main == "Mist") weatherIcon.src = "images/mist.png";
 };
 
 searchBtn.addEventListener("click", () => {
