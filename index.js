80602bac05bd033d8ac394025bde1ea7a74d04c7
Juan Quino <quinojuan@gmail.com>
2023-11-10 16:57:41 -0300

    dinamic data

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const checkWeather = async () => {
-  const response = await fetch(apiUrl + `&appid=${API_KEY}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const checkWeather = async (city) => {
+  const response = await fetch(apiUrl + city + `&appid=${API_KEY}`);
   var data = await response.json();
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
   console.log(data);
 };
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
